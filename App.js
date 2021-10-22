8846600c205e6e3285d5d3447d682748ccdbbb6b
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-10-22 15:44:23 -0500

    got cover image to display

--- App.js
+++ App.js
@@ -1,23 +1,37 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ImageBackground,
+} from "react-native";
 
 export default function App() {
-  console.log("lets see what happens");
-
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.containter}>
+      <ImageBackground
+        style={styles.image}
+        source={require("./assets/cover-photo.png")}
+      >
+        <Text styles={styles.overlayText}>Hello World how are you!</Text>
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containter: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
+  image: {
+    width: 415,
+    height: 950,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  overlayText: {},
 });
